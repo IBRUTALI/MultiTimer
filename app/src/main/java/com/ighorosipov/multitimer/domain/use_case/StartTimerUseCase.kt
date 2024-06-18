cfc419d45ba869cc11c87eaa3c2d065cd9e283ec
Igor Osipov <ighor.osipov.95@mail.ru>
2024-06-18 22:07:28 +0300

    Service bug fix.

--- app/src/main/java/com/ighorosipov/multitimer/domain/use_case/StartTimerUseCase.kt
+++ app/src/main/java/com/ighorosipov/multitimer/domain/use_case/StartTimerUseCase.kt
@@ -2,8 +2,6 @@ package com.ighorosipov.multitimer.domain.use_case
 
 import com.ighorosipov.multitimer.domain.model.Timer
 import kotlinx.coroutines.flow.flow
-import java.text.SimpleDateFormat
-import java.util.Locale
 import javax.inject.Inject
 
 class StartTimerUseCase @Inject constructor(
@@ -12,14 +10,11 @@ class StartTimerUseCase @Inject constructor(
 
     suspend operator fun invoke(timer: Timer) = flow {
         var startTime = timer.startTime
-        val format = SimpleDateFormat("HH:mm:ss", Locale.ENGLISH)
         while (startTime >= 0) {
             kotlinx.coroutines.delay(1000)
-            val date = java.util.Date(startTime)
             emit(
                 timer.copy(
                     startTime = startTime,
-                    timeString = format.format(date),
                     isRunning = true
                 )
             )
--- app/src/main/java/com/ighorosipov/multitimer/presentation/services/timer/TimerService.kt
+++ app/src/main/java/com/ighorosipov/multitimer/presentation/services/timer/TimerService.kt
@@ -6,28 +6,40 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 import com.ighorosipov.multitimer.R
-import com.ighorosipov.multitimer.di.DefaultDispatcher
+import com.ighorosipov.multitimer.di.IODispatcher
 import com.ighorosipov.multitimer.domain.model.Timer
 import com.ighorosipov.multitimer.domain.use_case.AddTimerUseCase
 import com.ighorosipov.multitimer.domain.use_case.DeleteTimerUseCase
 import com.ighorosipov.multitimer.domain.use_case.PauseTimerUseCase
 import com.ighorosipov.multitimer.domain.use_case.StartTimerUseCase
+import com.ighorosipov.multitimer.utils.base.safeCollect
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.timeout
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class TimerService : Service() {
 
-    @DefaultDispatcher
+    @IODispatcher
     @Inject
     lateinit var dispatcher: CoroutineDispatcher
 
@@ -48,8 +60,8 @@ class TimerService : Service() {
 
     private val binder = LocalBinder()
 
-    // Is the service tracking time?
-    private var isTimerRunning = false
+    // Is timer active?
+    private var isTimerRunning = AtomicBoolean(true)
 
     private var timerJob: Job? = null
 
@@ -63,59 +75,94 @@ class TimerService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        notificationBuilder = createNotification()
+        notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 CHANNEL_ID,
                 CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_HIGH
             )
-            notificationBuilder = createNotification()
-            startForeground(NOTIFICATION_ID, notificationBuilder?.build())
-            notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager?.createNotificationChannel(channel)
         }
-        return START_STICKY
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        notificationBuilder?.let { notificationBuilder ->
+            ServiceCompat.startForeground(
+                this,
+                NOTIFICATION_ID,
+                notificationBuilder.build(),
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
+                } else {
+                    0
+                })
+        }
+        return START_NOT_STICKY
     }
 
     fun startTimer(timer: Timer) {
+        timerJob?.cancel()
         timerJob = CoroutineScope(dispatcher).launch {
-            startTimerUseCase(timer).collect {
-                notificationBuilder?.setContentText(it.timeString)
-                notificationManager?.notify(NOTIFICATION_ID, notificationBuilder?.build())
-            }
+            startTimerUseCase(timer)
+                .transform {
+                    if (isTimerRunning.get()) {
+                        emit(it.copy(isRunning = true))
+                    }
+                    if (!isTimerRunning.get()) {
+                        while (!isTimerRunning.get() && isActive) {
+                            emit(it.copy(isRunning = false))
+                        }
+                    }
+                }
+                .collect {
+                    ensureActive()
+                    setTextToNotificationAndNotify(it.timeString)
+                }
         }
     }
 
     fun pauseTimer() {
-
+        isTimerRunning.set(!isTimerRunning.get())
     }
 
     fun stopTimer() {
         timerJob?.cancel()
-        removeNotification()
-    }
-
-    fun isTimerRunning(): Boolean {
-        return isTimerRunning
     }
 
     private fun createNotification(): NotificationCompat.Builder {
         val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Timer Active")
-            .setContentText("Tap to return to the timer")
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.drawable.ic_time)
+            .setContentTitle(this.resources.getString(R.string.timer_active))
             .setSilent(true)
-        val resultIntent = Intent(this, TimerService::class.java)
+            .setOngoing(true)
+
+        val resultIntent = Intent(
+            this,
+            TimerService::class.java
+        )
         val resultPendingIntent = PendingIntent.getActivity(
-            this, 0, resultIntent,
-            PendingIntent.FLAG_MUTABLE
+            this,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_IMMUTABLE
         )
         builder.setContentIntent(resultPendingIntent)
         return builder
+    }
+
+    private fun setTextToNotificationAndNotify(contextText: String) {
+        if (isTimerRunning.get()) {
+            notificationBuilder?.setContentTitle(this.resources.getString(R.string.timer_active))
+        }
+        if (!isTimerRunning.get()) {
+            notificationBuilder?.setContentTitle(this.resources.getString(R.string.timer_inactive))
+        }
+        notificationBuilder?.setContentText(contextText)
+        notificationManager?.notify(NOTIFICATION_ID, notificationBuilder?.build())
     }
 
     private fun removeNotification() {
@@ -124,14 +171,14 @@ class TimerService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
+        timerJob = null
         removeNotification()
-        stopTimer()
     }
 
     companion object {
-        const val NOTIFICATION_ID = 1
-        const val CHANNEL_ID = "MULTI_TIMER"
-        const val CHANNEL_NAME = "Multi timer"
+        const val NOTIFICATION_ID = 101
+        const val CHANNEL_ID = "MULTI_TIMER_CHANNEL_TIMER"
+        const val CHANNEL_NAME = "MULTI_TIMER_NAME_TIMER"
     }
 
 }
